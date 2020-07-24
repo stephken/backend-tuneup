18c45a10d562b3619c7905ec0a7290c0689405ab
ken stephens <kenstephens33@gmail.com>
2020-07-24 00:08:38 -0400

    final code

--- tuneup.py
+++ tuneup.py
@@ -22,15 +22,14 @@ def profile(func):
         result = func(*args, **kwargs)
         performance_object.disable()
 
-        get_stats_obj = pstats.Stats(performance_object)\
-        .strip_dirs()\
-        .sort_stats('cumulative')\
-        .print_stats()
+    
         get_stats_obj = pstats.Stats(performance_object).strip_dirs().sort_stats('cumulative').print_stats()
 
         return result
 
     return profile_wrapper
+
+
 def read_movies(src):
     """Returns a list of movie titles."""
     print(f'Reading file: {src}')
